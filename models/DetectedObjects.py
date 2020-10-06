ff4ea51566b4a6ff153dec671feb8ae38e9cc397
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2020-10-06 18:43:50 -0300

    Adiciona dimensões da imagem e converte coordenadas para pixels

--- models/DetectedObjects.py
+++ models/DetectedObjects.py
@@ -1,7 +1,10 @@
 from .Object import Object
 
+
 class DetectedObjects:
-    def __init__(self, boxes, scores, classes, num, category_index, path, shape) -> None:
+    def __init__(
+        self, boxes, scores, classes, num, category_index, path, shape
+    ) -> None:
         self._boxes = boxes
         self._scores = scores
         self._classes = classes
@@ -11,62 +14,70 @@ class DetectedObjects:
         self._height = shape[0]
         self._width = shape[1]
         self._depth = shape[2]
-        
+
         self._objects = self._create_objects()
-    
+
     @property
     def path(self):
         return self._path
-    
+
     @property
     def height(self):
         return self._height
-    
+
     @property
     def width(self):
         return self._width
-    
+
     @property
     def depth(self):
         return self._depth
-    
+
     @property
     def objects(self):
         return self._objects
-         
+
     def _create_objects(self):
         objects = []
-        for box, score, class_ in zip(self._boxes[0], self._scores[0], self._classes[0]):
+        for box, score, class_ in zip(
+            self._boxes[0], self._scores[0], self._classes[0]
+        ):
             class_string = str(int(class_))
             category = self._category_index[class_string]
             object = Object(box, score, category)
             objects.append(object)
-        
+
         return objects
 
     def to_json(self):
-        json_objects = {'objects': []}
+        json_objects = {
+            "image": {
+                "width": self._width,
+                "height": self._height,
+                "depth": self._depth,
+            },
+            "objects": [],
+        }
         objects = self._objects
         for obj in objects:
-            box_raw = [str(i) for i in list(obj.box)]
+            box_raw = list(obj.box)
             box = {
-                'ymin': box_raw[0],
-                'xmin': box_raw[1],
-                'ymax': box_raw[2],
-                'xmax': box_raw[3],
+                "ymin": str(box_raw[0] * self._height),
+                "xmin": str(box_raw[1] * self._width),
+                "ymax": str(box_raw[2] * self._height),
+                "xmax": str(box_raw[3] * self._width),
             }
-            
+
             json_object = {
-                'name': str(obj),
-                'box': box,
-                'score': str(obj.score),
-                'x': str(obj.x),
-                'y': str(obj.y),
-                'height': str(obj.height),
-                'width':str(obj.width)
+                "name": str(obj),
+                "box": box,
+                "score_percentage": str(obj.score * 100),
+                "x_center": str(obj.x * self._width),
+                "y_center": str(obj.y * self.height),
+                "box_height": str(obj.height * self.height),
+                "box_width": str(obj.width * self._width),
             }
-        
-            json_objects['objects'].append(json_object)
+
+            json_objects["objects"].append(json_object)
 
         return json_objects
-        
