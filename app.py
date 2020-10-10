d8a1411a39ca8bed1666a7bd92b151921051d94a
Jefferson <jefferson.bizarri@gmail.com>
2020-10-10 15:56:16 -0300

    Corrige mensagem de erro

--- app.py
+++ app.py
@@ -8,28 +8,31 @@ from models.SsdMobileNet import SsdMobileNet
 
 app = Flask(__name__)
 
-RES_FOLDER = 'resources'
+RES_FOLDER = "resources"
 
 @app.route("/api/v1/mobilenet", methods=["POST"])
 def mobilenet():
     hex_string = str(uuid.uuid4().hex)
     filename = f"{hex_string}.jpg"
 
-    path = os.path.join('resources', filename)
+    path = os.path.join("resources", filename)
     with open(path, "wb") as file:
         file.write(request.data)
 
     try:
         objects = SsdMobileNet(path).run()
         status = 200
-        response = {'status': status, 'body': objects}
+        response = {"status": status, "body": objects}
     except Exception as e:
         status = 500
-        response = {'status': status, 'body': e}
+        response = {"status": status, "body": e}
     finally:
         os.remove(path)
-        
-    return Response(response=json.dumps(response), status=status, mimetype="application/json")
+
+    return Response(
+        response=json.dumps(response), status=status, mimetype="application/json"
+    )
+
 
 def create_app():
     global app
